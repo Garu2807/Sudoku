eb378f4a67d7e7077627bd5e51a33f339d4a021b
Алина Шнайдер <shnaideralina@MacBook-Air-Alina.local>
2023-06-02 16:05:19 +0300

    solve

--- sudoku.js
+++ sudoku.js
@@ -1,28 +1,28 @@
+const { log } = require('console');
+
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
 function solve(boardString) {
+  const b = boardString.split('');
+  let size = 9;
+  let subarray = [];
+  for (let i = 0; i < Math.ceil(b.length / size); i++) {
+    subarray[i] = b.slice(i * size, i * size + size);
+  }
+  return subarray;
 }
+console.log(solve(boardString))
 
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает булевое значение — решено это игровое поле или нет.
- */
-function isSolved(board) {
-
-}
-
-/**
- * Принимает игровое поле в том формате, в котором его вернули из функции solve.
- * Возвращает строку с игровым полем для последующего вывода в консоль.
- * Подумай, как симпатичнее сформировать эту строку.
- */
-function prettyBoard(board) {
+function isSolved(board){
 
 }
 
+function prettyBoard(board){
+
+}
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
   solve,
